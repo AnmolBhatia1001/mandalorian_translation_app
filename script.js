0d58b021b530c0836b7706e4c2d3800db50e0fbe
AnmolSinghBhatia1001 <anmolsinghbhatia1001@gmail.com>
2022-09-10 08:01:35 +0530

    Functionality Added

--- script.js
+++ script.js
@@ -3,4 +3,12 @@
 var userInput = document.querySelector("#userInput");
 var convertBtn = document.querySelector("button");
 var outputDiv = document.querySelector("#output");
+var apiURL = "https://api.funtranslations.com/translate/mandalorian.json";
+
+convertBtn.addEventListener("click", ()=>{
+    var reqURL = `${apiURL}?text=${userInput.value}`
+    fetch(encodeURI(reqURL))
+    .then( (response) => response.json())
+    .then( (output) => outputDiv.innerText = output.contents.translated)
+})
 
